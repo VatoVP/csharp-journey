c65d288c430e3162f5907537079e3cddcc74a445
Pirvella <vakhtangpirveli@gmail.com>
2026-08-04 00:08:28 +0400

    Add initial solution, project, and Car class implementation
    Created a new Visual Studio solution (`01-Classes.sln`) and
    project file (`01-Classes.csproj`) targeting .NET 8.0. Added
    a `Car` class with fields (`Brand`, `Model`, `Year`) and
    methods (`Start`, `Drive`, `ShowInfo`). Implemented a
    `Program.cs` file to demonstrate the functionality of the
    `Car` class with two instances.

---
+++ 01-Classes/01-Classes/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Classes
+{
+    class Car
+    {
+        public string Brand;
+        public string Model;
+        public int Year;
+
+        public void Start()
+        {
+            Console.WriteLine("The car started");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("The Car is driving");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} Model: {Model} Year: {Year}");
+        }
+    }
+}
---
+++ 01-Classes/01-Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Brand = "Toyota";
+            car1.Model = "Camry";
+            car1.Year = 2020;
+            car1.Start();    
+            car1.Drive();
+            car1.ShowInfo();
+
+            Car car2 = new Car();
+            car2.Brand = "Honda";
+            car2.Model = "Civic";
+            car2.Year = 2021;
+            car2.Start();
+            car2.Drive();
+            car2.ShowInfo();
+
+        }
+    }
+}
